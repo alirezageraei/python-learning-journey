ba62252ff4721e0244c76aac4756586e25b7af23
Alireza Geraei <alirezageraei0@gmail.com>
2026-06-06 19:27:29 +0330

    Project updated: main.py is almost completed

--- Student-Management-System/main.py
+++ Student-Management-System/main.py
@@ -1,4 +1,5 @@
 import student
+import json
 
 # -------------------------------------------------------- #
 def get_info():
@@ -34,7 +35,7 @@ def add_student(student_obj, append_to):
 
     # adding student object to the list
     append_to.append(student_obj)
-    print(f"{student_obj.name} is Added to students list\n" + "-"*30)
+    print(30*"-" + f"\n{student_obj.name} is Added to students list\n" + "-"*30)
 
 
 # ---------------------------------------------------------------- #
@@ -51,14 +52,82 @@ while True:
     print("5. Save Data")
     print("6. Exit")
 
-    option = int(input("Choose an Option(just the number): "))
+    try:
+        option = int(input("Choose an Option between 1 - 6\n(just the number): "))
+    
+    except ValueError:
+        print("Invalid Input")
+        continue
+    
     # if option.upper() in ["6","EXIT","6. EXIT","6.EXIT"]:
     if option == 6:
         break
     elif option == 1:
-        student_info = get_info()
-        student_obj = create_student(student_info[0],student_info[1],student_info[2])
-        add_student(student_obj, std_list)
+        while True:
+            student_info = get_info()
+            student_obj = create_student(student_info[0],student_info[1],student_info[2])
+            add_student(student_obj, std_list)
+            repeat = input("Enter '+' to do another adding\nor press Enter for main menu ")
+            if repeat != "+":
+                break
+
+    elif option == 2:
+        while True:
+            for student in std_list:
+                # print(f"name: {student.name}\nage: {student.age}\ngrade: {student.grade}\n" + "***" * 10)
+                print(student)
+            repeat = input("Enter '+' to see informations again\nor press Enter for main menu ")
+            if repeat != "+":
+                    break
+            
+    elif option == 3:
+
+        names_list = [student.name for student in std_list]
+
+        while True:
+            searched_name = input("Enter the name:_________")
+            
+            if searched_name in names_list:
+                print(f"The name {searched_name} is in the list\n-------------------")
+            else:
+                print(f"name {searched_name} is not exist\n----------------------")
+            repeat = input("Enter '+' to search again\nor press Enter for main menu ")
+            if repeat != "+":
+                    break
+
+    elif option == 4:
+        found = False
+        while True:
+            Id = int(input("Enter the id you want to remove: "))
+            for student in std_list:
+                if Id == student.id:
+                    std_list.remove(student)
+                    print(f"name {student.name} is removed\n---------------------")
+                    found = True
+            if not found:
+                print("this Id is not exist\n------------------------")
+            repeat = input("Enter '+' to delete another item\nor press Enter for main menu ")
+            if repeat != "+":
+                    break
+            
+    elif option == 5:
+
+        with open("students.json" , "w") as std:
+            save_list = []
+            for student in std_list:
+                data = {
+                    "id": student.id,
+                    "name": student.name,
+                    "age": student.age,
+                    "grade": student.grade
+                    }
+                save_list.append(data)
+            json.dump(
+                save_list,
+                std,
+                indent = 4
+                )
+        print("data saved successfully")
     else:
         # raise TypeError("Entered Value must be an Integer")
         break # فعلا این باشه تا آخرسر
--- Student-Management-System/student.py
+++ Student-Management-System/student.py
@@ -2,8 +2,20 @@ class Student:
     """
     
     """
+
+    first_id = 1
     def __init__(self, name, age, grade):
         self.name = name
         self.age = age
         self.grade = grade
-        self.id = None
+        
+        self.id = Student.first_id
+        Student.first_id += 1
+
+    def __str__(self):
+        return (
+            f"name: {self.name}\n"
+            f"age: {self.age}\n"
+            f"grade: {self.grade}\n"
+            "-------------------------------"
+            )
